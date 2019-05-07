70cda5b71427dddc7fc31581835adcf16255996e
sarab <sarabjeet@ucreate.co.in>
2019-05-07 16:03:06 +0530

    initial redux saga code

---
+++ src/containers/NewsItem.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+
+const imgStyle = {
+  hight: 'auto',
+  width: '80%',
+  border: '4px solid RebeccaPurple ',
+  borderRadius: '5%'
+};
+const articleStyle = {
+  width: '50%',
+  margin: '0 auto',
+  color: 'olive'
+}
+
+class NewsItem extends React.PureComponent {
+  render = () => {
+    const { article } = this.props;
+    return article ?
+        <article style={articleStyle} >
+          <div>
+            <h1>{article.title}</h1>
+            <img style={imgStyle} src={article.urlToImage} alt="" />
+            <h2>{article.description}</h2>
+            <a href={article.url} target="_blank">READ MORE</a>
+          </div>
+        </article> :
+        null
+  }
+}
+
+const mapStateToProps = (state) => ({
+  article: state.news,
+})
+
+export default connect(
+  mapStateToProps,
+  null
+)(NewsItem)
+
+
+
